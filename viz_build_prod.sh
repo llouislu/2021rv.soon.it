9b8651658cdd72652c105a22044d2c299d651737
llouislu <louis.dev@outlook.com>
2023-09-19 19:33:23 +1200

    fixed ng build for prod

--- viz_build_prod.sh
+++ viz_build_prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec viz /bin/bash -c "/usr/local/bin/npm install && /usr/local/bin/npm run-script build"
+docker exec viz /bin/bash -c "NPM_CONFIG_PREFIX=~/.npm-global /usr/local/bin/npm install && /usr/local/bin/npm run-script build"
